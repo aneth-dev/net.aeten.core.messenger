4c6fee489224d4c0f49e5cf25233635da0d8e1ee
Thomas Pérennou <thomas@thomas-laptop.(none)>
2010-02-03 23:24:58 +0100

    Remove IDE files

--- .fr-ekvpx8/org.pititom.core/src/org/pititom/core/stream/extension/StreamEditor.java
+++
@@ -1,9 +0,0 @@
-package org.pititom.core.stream.extension;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-public interface StreamEditor {
-	public void edit(DataInputStream in, DataOutputStream out) throws IOException;
-}
