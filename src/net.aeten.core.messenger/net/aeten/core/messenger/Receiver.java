42c74ea7f30f03468379b0bec94bdd73dcee0505
Thomas Pérennou <thomas.perennou@aeten.net>
2012-10-26 11:30:26 +0100

    The method isConnected must be thread safe

--- src/net.aeten.core.messenger/net/aeten/core/messenger/Receiver.java
+++ src/net.aeten.core.messenger/net/aeten/core/messenger/Receiver.java
@@ -6,6 +6,10 @@ import net.aeten.core.Identifiable;
 
 public interface Receiver<Message> extends Identifiable, Connection {
 	public void receive(MessengerEventData<Message> data) throws IOException;
+	
+	/** Must be thread safe */
+	@Override
+	public boolean isConnected();
 
 	public static abstract class ReceiverAdapter<Message> extends ConnectionAdapter implements Receiver<Message> {
 		public ReceiverAdapter(String identifier) {
