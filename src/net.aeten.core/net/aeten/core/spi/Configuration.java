07c619df49dbc7d8360b084cdeb5a3dd65ada19d
Thomas Pérennou <thomas.perennou@aeten.net>
2012-10-26 11:24:17 +0100

    Add Documented, Retention and Target annotations

--- src/net.aeten.core/net/aeten/core/spi/Configuration.java
+++ src/net.aeten.core/net/aeten/core/spi/Configuration.java
@@ -1,9 +1,18 @@
 package net.aeten.core.spi;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  *
  * @author Thomas Pérennou
  */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.PACKAGE, ElementType.TYPE})
 public @interface Configuration {
 	String name();
 	Class<?> provider();
