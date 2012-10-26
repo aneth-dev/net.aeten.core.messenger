67955bc036a6d719ddf122c35920e3d2a46a5b23
Thomas Pérennou <thomas.perennou@aeten.net>
2012-10-26 11:18:50 +0100

    Add getter and setter methods.

--- src/net.aeten.core/net/aeten/core/Model.java
+++ src/net.aeten.core/net/aeten/core/Model.java
@@ -3,8 +3,13 @@ package net.aeten.core;
 import net.aeten.core.event.EventData;
 import net.aeten.core.event.Handler;
 
+public interface Model <M extends Model<M, F>, F extends Enum <F>> {
+	void addObserver (Handler <EventData <M, F>> observer);
 
-public interface Model<M, F extends Enum<F>> {
-	void addObserver(Handler<EventData<M, F>> observer);
-	void removeObserver(Handler<EventData<M, F>> observer);
+	void removeObserver (Handler <EventData <M, F>> observer);
+
+	<T> void set (	F field,
+						T value);
+
+	<T> T get (F field);
 }
