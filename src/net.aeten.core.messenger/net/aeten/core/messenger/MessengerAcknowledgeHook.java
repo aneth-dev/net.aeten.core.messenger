b44d547ad007c3e590c13ad065e01a8c08e4acc4
Thomas Pérennou <thomas.perennou@aeten.net>
2012-11-02 13:08:23 +0000

    Remove logging module and use SLF4J instead

--- src/net.aeten.core.messenger/net/aeten/core/messenger/MessengerAcknowledgeHook.java
+++ src/net.aeten.core.messenger/net/aeten/core/messenger/MessengerAcknowledgeHook.java
@@ -8,58 +8,66 @@ import net.aeten.core.event.Handler;
 import net.aeten.core.event.HandlerRegister;
 import net.aeten.core.event.TransmitterFactory;
 import net.aeten.core.event.TransmitterService;
-import net.aeten.core.logging.LogLevel;
-import net.aeten.core.logging.Logger;
 import net.aeten.core.spi.FieldInit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 
  * @author Thomas Pérennou
  */
-public class MessengerAcknowledgeHook<Message, Acknowledge extends Enum<?>> implements Handler<MessengerEventData<Message>>, HandlerRegister<MessengerAcknowledgeEvent, MessengerAcknowledgeEventData<Message, Acknowledge>>, Identifiable {
+public class MessengerAcknowledgeHook <Message, Acknowledge extends Enum <?>> implements
+		Handler <MessengerEventData <Message>>,
+		HandlerRegister <MessengerAcknowledgeEvent, MessengerAcknowledgeEventData <Message, Acknowledge>>,
+		Identifiable {
 
-	private static final Map<String, Object> MUTEX_MAP = new HashMap<>(1);
+	private static final Logger LOGGER = LoggerFactory.getLogger (MessengerAcknowledgeHook.class);
+	private static final Map <String, Object> MUTEX_MAP = new HashMap <> (1);
 
 	@FieldInit
 	private String identifier;
 
-	@FieldInit(alias = "acknowledge protocol", required = false)
-	private Class<? extends MessengerAcknowledgeProtocol<Message, Acknowledge>> acknowledgeProtocolClass;
+	@FieldInit (alias = "acknowledge protocol",
+					required = false)
+	private Class <? extends MessengerAcknowledgeProtocol <Message, Acknowledge>> acknowledgeProtocolClass;
 
-	@FieldInit(alias = "acknowledge protocol configuration", required = false)
+	@FieldInit (alias = "acknowledge protocol configuration",
+					required = false)
 	private String acknowledgeProtocolConfiguration;
 
-	private MessengerAcknowledgeProtocol<Message, Acknowledge> acknowledgeProtocol = null;
-	private Object acknowledgeMutex = new Object();
+	private MessengerAcknowledgeProtocol <Message, Acknowledge> acknowledgeProtocol = null;
+	private Object acknowledgeMutex = new Object ();
 
-	private volatile MessengerAcknowledgeEventData<Message, Acknowledge> currentEventData = null;
+	private volatile MessengerAcknowledgeEventData <Message, Acknowledge> currentEventData = null;
 
 	// private RegisterableTransmitter<MessengerAcknowledgeEvent,
 	// MessengerAcknowledgeEventData<Message, Acknowledge>> eventTransmitter =
 	// null;
-	private TransmitterService<MessengerAcknowledgeEvent, MessengerAcknowledgeEventData<Message, Acknowledge>> eventTransmitter = null;
+	private TransmitterService <MessengerAcknowledgeEvent, MessengerAcknowledgeEventData <Message, Acknowledge>> eventTransmitter = null;
 
-	public MessengerAcknowledgeHook(String identifier) {
-		this(identifier, null, null);
+	public MessengerAcknowledgeHook (String identifier) {
+		this (identifier, null, null);
 	}
 
 //	public MessengerAcknowledgeHook(@SpiInitializer MessengerAcknowledgeHookInit init) {
 //		this(init.getId(), init.hasAcknowledgeProtocolClass() ? init.getAcknowledgeProtocolClass(): null, init.hasAcknowledgeProtocolConfiguration() ? init.getAcknowledgeProtocolConfiguration(): null);
 //	}
 
-	public MessengerAcknowledgeHook(String identifier, String description, MessengerAcknowledgeProtocol<Message, Acknowledge> acknowledgeProtocol) {
-		this.identifier = (identifier == null) ? this.getClass().getName() : identifier;
+	public MessengerAcknowledgeHook (String identifier,
+												String description,
+												MessengerAcknowledgeProtocol <Message, Acknowledge> acknowledgeProtocol) {
+		this.identifier = (identifier == null)? this.getClass ().getName (): identifier;
 		this.acknowledgeProtocol = acknowledgeProtocol;
-		this.eventTransmitter = TransmitterFactory.asynchronous("Messenger acknowledge hook \"" + this + "\" event transmitter", MessengerAcknowledgeEvent.values());
+		this.eventTransmitter = TransmitterFactory.asynchronous ("Messenger acknowledge hook \"" + this + "\" event transmitter", MessengerAcknowledgeEvent.values ());
 		// this.eventTransmitter =
 		// TransmitterFactory.synchronous(MessengerAcknowledgeEvent.values());
 
 		try {
-			this.acknowledgeMutex = MUTEX_MAP.get(this.identifier);
+			this.acknowledgeMutex = MUTEX_MAP.get (this.identifier);
 			if (this.acknowledgeMutex == null) {
-				this.acknowledgeMutex = new Object();
-				MUTEX_MAP.put(this.identifier, this.acknowledgeMutex);
+				this.acknowledgeMutex = new Object ();
+				MUTEX_MAP.put (this.identifier, this.acknowledgeMutex);
 			}
 
 //			this.acknowledgeProtocol = acknowledgeProtocolClass.newInstance();
@@ -67,94 +75,96 @@ public class MessengerAcknowledgeHook<Message, Acknowledge extends Enum<?>> impl
 //				((Configurable<String>) this.acknowledgeProtocol).configure(this.acknowledgeProtocolConfiguration);
 //			}
 
-			this.eventTransmitter = TransmitterFactory.asynchronous("Messenger acknowledge hook \"" + this.getIdentifier() + "\" event transmitter", MessengerAcknowledgeEvent.values());
+			this.eventTransmitter = TransmitterFactory.asynchronous ("Messenger acknowledge hook \"" + this.getIdentifier () + "\" event transmitter", MessengerAcknowledgeEvent.values ());
 			// this.eventTransmitter =
 			// TransmitterFactory.synchronous(MessengerAcknowledgeEvent.values());
 		} catch (Exception exception) {
-			throw new IllegalArgumentException(exception);
+			throw new IllegalArgumentException (exception);
 		}
 
 	}
 
 	@Override
-	public void handleEvent(MessengerEventData<Message> data) {
-		switch (data.getEvent().getSourceEvent()) {
+	public void handleEvent (MessengerEventData <Message> data) {
+		switch (data.getEvent ().getSourceEvent ()) {
 		case SEND:
-			this.sendHook(data);
+			this.sendHook (data);
 			break;
 		case RECEIVE:
-			this.startReception(data);
+			this.startReception (data);
 			break;
 		default:
 			break;
 		}
 	}
 
-	private void sendHook(MessengerEventData<Message> eventData) {
-		this.currentEventData = new MessengerAcknowledgeEventData<Message, Acknowledge>(eventData.getSource(), null, eventData.getMessage(), null, null);
-		long timeOut = this.acknowledgeProtocol.getAcknowledgedTimeout(eventData.getMessage());
+	private void sendHook (MessengerEventData <Message> eventData) {
+		currentEventData = new MessengerAcknowledgeEventData <Message, Acknowledge> (eventData.getSource (), null, eventData.getMessage (), null, null);
+		long timeOut = acknowledgeProtocol.getAcknowledgedTimeout (eventData.getMessage ());
 		if (timeOut == 0) {
 			return;
 		}
 		try {
 
-			synchronized (this.acknowledgeMutex) {
-				this.acknowledgeMutex.wait(timeOut);
+			synchronized (acknowledgeMutex) {
+				acknowledgeMutex.wait (timeOut);
 			}
 
 			MessengerAcknowledgeEvent notificationEvent;
-			if (this.currentEventData.getRecievedMessage() == null) {
+			if (currentEventData.getRecievedMessage () == null) {
 				notificationEvent = MessengerAcknowledgeEvent.UNACKNOWLEDGED;
 			} else {
-				final boolean success = (this.currentEventData.getAcknowledge() == null) ? false : this.acknowledgeProtocol.isSuccess(this.currentEventData.getAcknowledge());
-				notificationEvent = success ? MessengerAcknowledgeEvent.ACKNOWLEDGED : MessengerAcknowledgeEvent.UNACKNOWLEDGED;
+				final boolean success = (currentEventData.getAcknowledge () == null)? false: acknowledgeProtocol.isSuccess (this.currentEventData.getAcknowledge ());
+				notificationEvent = success? MessengerAcknowledgeEvent.ACKNOWLEDGED: MessengerAcknowledgeEvent.UNACKNOWLEDGED;
 			}
 
-			this.eventTransmitter.transmit(new MessengerAcknowledgeEventData<Message, Acknowledge>(this.currentEventData.getSource(), notificationEvent, this.currentEventData.getSentMessage(), this.currentEventData.getRecievedMessage(), this.currentEventData.getAcknowledge()));
+			eventTransmitter.transmit (new MessengerAcknowledgeEventData <Message, Acknowledge> (currentEventData.getSource (), notificationEvent, this.currentEventData.getSentMessage (), this.currentEventData.getRecievedMessage (), this.currentEventData.getAcknowledge ()));
 
 		} catch (Exception exception) {
-			Logger.log(this.currentEventData.getSource(), LogLevel.ERROR, exception);
+			LOGGER.error (currentEventData.getSource ().toString (), exception);
 		}
 	}
 
-	private void startReception(MessengerEventData<Message> eventData) {
+	private void startReception (MessengerEventData <Message> eventData) {
 		if (this.currentEventData != null) {
 			try {
-				final Acknowledge acknowledge = this.acknowledgeProtocol.getAcknowledge(this.currentEventData.getSentMessage(), eventData.getMessage());
+				final Acknowledge acknowledge = this.acknowledgeProtocol.getAcknowledge (this.currentEventData.getSentMessage (), eventData.getMessage ());
 
 				if (acknowledge != null) {
-					this.currentEventData.setRecievedMessage(eventData.getMessage());
-					this.currentEventData.setAcknowledge(acknowledge);
+					this.currentEventData.setRecievedMessage (eventData.getMessage ());
+					this.currentEventData.setAcknowledge (acknowledge);
 					synchronized (this.acknowledgeMutex) {
-						this.acknowledgeMutex.notifyAll();
+						this.acknowledgeMutex.notifyAll ();
 					}
 				}
 
 			} catch (Exception exception) {
-				Logger.log(this.currentEventData.getSource(), LogLevel.ERROR, exception);
+				LOGGER.error (currentEventData.getSource ().toString (), exception);
 			}
 		}
 	}
 
 	@Override
-	public final String getIdentifier() {
+	public final String getIdentifier () {
 		return this.identifier;
 	}
 
 	@Override
-	public String toString() {
+	public String toString () {
 		return this.identifier;
 	}
 
 	@Override
-	public void addEventHandler(Handler<MessengerAcknowledgeEventData<Message, Acknowledge>> eventHandler, MessengerAcknowledgeEvent... eventList) {
-		this.eventTransmitter.addEventHandler(eventHandler, eventList);
+	public void addEventHandler (	Handler <MessengerAcknowledgeEventData <Message, Acknowledge>> eventHandler,
+											MessengerAcknowledgeEvent... eventList) {
+		this.eventTransmitter.addEventHandler (eventHandler, eventList);
 
 	}
 
 	@Override
-	public void removeEventHandler(Handler<MessengerAcknowledgeEventData<Message, Acknowledge>> eventHandler, MessengerAcknowledgeEvent... eventList) {
-		this.eventTransmitter.removeEventHandler(eventHandler, eventList);
+	public void removeEventHandler (	Handler <MessengerAcknowledgeEventData <Message, Acknowledge>> eventHandler,
+												MessengerAcknowledgeEvent... eventList) {
+		this.eventTransmitter.removeEventHandler (eventHandler, eventList);
 	}
 
 }
--- test/net.aeten.core.messenger.test/net/aeten/core/messenger/test/MessengerTest.java
+++ test/net.aeten.core.messenger.test/net/aeten/core/messenger/test/MessengerTest.java
@@ -1,13 +1,8 @@
 package net.aeten.core.messenger.test;
 
-import java.util.Calendar;
-import java.util.Date;
 import net.aeten.core.event.Handler;
 import net.aeten.core.event.Hook;
 import net.aeten.core.event.Priority;
-import net.aeten.core.logging.LogLevel;
-import net.aeten.core.logging.Logger;
-import net.aeten.core.logging.LoggingData;
 import net.aeten.core.messenger.Messenger;
 import net.aeten.core.messenger.MessengerAcknowledgeEvent;
 import net.aeten.core.messenger.MessengerAcknowledgeEventData;
@@ -20,80 +15,71 @@ import net.aeten.core.spi.Configuration;
 import net.aeten.core.spi.Configurations;
 import net.aeten.core.spi.Service;
 
-@Configurations({
-	@Configuration(name = "server.yaml",
-						provider = MessengerProvider.class),
-	@Configuration(name = "client.yaml",
-						provider = MessengerProvider.class,
-						parser = "net.aeten.core.parsing.yaml.YamlParser"),
-	@Configuration(name = "client.receiver.yaml",
-						provider = UdpIpReceiver.class)})
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Configurations ({
+		@Configuration (	name = "server.yaml",
+								provider = MessengerProvider.class),
+		@Configuration (	name = "client.yaml",
+								provider = MessengerProvider.class,
+								parser = "net.aeten.core.parsing.yaml.YamlParser"),
+		@Configuration (	name = "client.receiver.yaml",
+								provider = UdpIpReceiver.class)
+})
 public class MessengerTest {
 
-	@SuppressWarnings("unchecked")
-	public static void main(String[] args) throws Exception {
-		Logger.addEventHandler(new Handler<LoggingData>() {
+	private static Logger LOGGER = LoggerFactory.getLogger (MessengerTest.class);
+
+	@SuppressWarnings ("unchecked")
+	public static void main (String[] args) throws Exception {
+
+		LOGGER.info ("Start");
+
+		Messenger <AbstractMessage> client = Service.getProvider (Messenger.class, "net.aeten.core.test.messenger.client");
+		Messenger <AbstractMessage> server = Service.getProvider (Messenger.class, "net.aeten.core.test.messenger.server");
+
+		AcknowledgeProtocol protocol = new AcknowledgeProtocol ();
+		MessengerAcknowledgeHook <AbstractMessage, Acknowledge> clientAcknowledgeHook = new MessengerAcknowledgeHook <> ("Client acknowledge hook test", null, protocol);
+		MessengerAcknowledgeHook <AbstractMessage, Acknowledge> serverAcknowledgeHook = new MessengerAcknowledgeHook <> ("Server acknowledge hook test", null, protocol);
+		server.addEventHandler (serverAcknowledgeHook, Messenger.EVENTS.get (MessengerEvent.SEND, Hook.POST), Messenger.EVENTS.get (MessengerEvent.RECEIVE, Hook.POST));
+		client.addEventHandler (clientAcknowledgeHook, Messenger.EVENTS.get (MessengerEvent.SEND, Hook.POST), Messenger.EVENTS.get (MessengerEvent.RECEIVE, Hook.POST));
+
+		Handler <MessengerAcknowledgeEventData <AbstractMessage, Acknowledge>> acknowledgeEventHandler = new Handler <MessengerAcknowledgeEventData <AbstractMessage, Acknowledge>> () {
 
 			@Override
-			public void handleEvent(LoggingData data) {
-				/*
-				 * Basic logging. Can be plugged to anyone logging tool.
-				 * net.aeten.core.messenger.Messenger exceptions can be caught by
-				 * this way
-				 */
-				synchronized (this) {
-					Date date = Calendar.getInstance().getTime();
-
-					System.out.println(date + String.format(" %3dms %5s  source={%s} %s%s", date.getTime() % 1000, data.getEvent(), data.getSource(), data.getMessage(), (data.getThrowable() == null) ? "" : ": "));
-					if (data.getThrowable() != null) {
-						data.getThrowable().printStackTrace(System.out);
-					}
+			public void handleEvent (MessengerAcknowledgeEventData <AbstractMessage, Acknowledge> data) {
+				String message = "source=({}); event=({}) ; eventData=({})";
+				if (data.getEvent () == MessengerAcknowledgeEvent.ACKNOWLEDGED) {
+					LOGGER.info (message, data.getSource (), data.getEvent (), data);
+				} else {
+					LOGGER.error (message, data.getSource (), data.getEvent (), data);
 				}
-			}
-		}, LogLevel.values());
-
-		Logger.log(MessengerTest.class, LogLevel.INFO, "Start");
-
-		Messenger<AbstractMessage> client = Service.getProvider(Messenger.class, "net.aeten.core.test.messenger.client");
-		Messenger<AbstractMessage> server = Service.getProvider(Messenger.class, "net.aeten.core.test.messenger.server");
-
-		AcknowledgeProtocol protocol = new AcknowledgeProtocol();
-		MessengerAcknowledgeHook<AbstractMessage, Acknowledge> clientAcknowledgeHook = new MessengerAcknowledgeHook<>("Client acknowledge hook test", null, protocol);
-		MessengerAcknowledgeHook<AbstractMessage, Acknowledge> serverAcknowledgeHook = new MessengerAcknowledgeHook<>("Server acknowledge hook test", null, protocol);
-		server.addEventHandler(serverAcknowledgeHook, Messenger.EVENTS.get(MessengerEvent.SEND, Hook.POST), Messenger.EVENTS.get(MessengerEvent.RECEIVE, Hook.POST));
-		client.addEventHandler(clientAcknowledgeHook, Messenger.EVENTS.get(MessengerEvent.SEND, Hook.POST), Messenger.EVENTS.get(MessengerEvent.RECEIVE, Hook.POST));
-
-		Handler<MessengerAcknowledgeEventData<AbstractMessage, Acknowledge>> acknowledgeEventHandler = new Handler<MessengerAcknowledgeEventData<AbstractMessage, Acknowledge>>() {
-
-			@Override
-			public void handleEvent(MessengerAcknowledgeEventData<AbstractMessage, Acknowledge> data) {
-				LogLevel level = (data.getEvent() == MessengerAcknowledgeEvent.ACKNOWLEDGED) ? LogLevel.INFO : LogLevel.ERROR;
-				Logger.log(data.getSource(), level, "event={" + data.getEvent() + "}; eventData={" + data + "}");
 			}
 		};
-		clientAcknowledgeHook.addEventHandler(acknowledgeEventHandler, MessengerAcknowledgeEvent.values());
-		serverAcknowledgeHook.addEventHandler(acknowledgeEventHandler, MessengerAcknowledgeEvent.values());
+		clientAcknowledgeHook.addEventHandler (acknowledgeEventHandler, MessengerAcknowledgeEvent.values ());
+		serverAcknowledgeHook.addEventHandler (acknowledgeEventHandler, MessengerAcknowledgeEvent.values ());
 
-		server.addEventHandler(new Handler<MessengerEventData<AbstractMessage>>() {
+		server.addEventHandler (new Handler <MessengerEventData <AbstractMessage>> () {
 
 			@Override
-			public void handleEvent(MessengerEventData<AbstractMessage> data) {
-				if (data.getMessage() instanceof AcknowledgeMessage) {
+			public void handleEvent (MessengerEventData <AbstractMessage> data) {
+				if (data.getMessage () instanceof AcknowledgeMessage) {
 					return;
 				}
-				if (data.getMessage().getAcknowledge() == null) {
-					data.getSource().transmit(new AcknowledgeMessage(Acknowledge.INVALID_MESSAGE), "net.aeten.core.test.messenger.server.sender");
+				if (data.getMessage ().getAcknowledge () == null) {
+					data.getSource ().transmit (new AcknowledgeMessage (Acknowledge.INVALID_MESSAGE), "net.aeten.core.test.messenger.server.sender");
 					return;
 				}
-				switch (data.getMessage().getAcknowledge()) {
+				switch (data.getMessage ().getAcknowledge ()) {
 				case SOLLICITED_NEED_ACKNOWLEDGE:
 				case UNSOLLICITED_NEED_ACKNOWLEDGE:
-					if (data.getMessage() instanceof Message) {
-						Message recievedMessage = (Message) data.getMessage();
-						if ((recievedMessage.getValue() < Message.MIN_VALUE) || (recievedMessage.getValue() > Message.MAX_VALUE)) {
-							data.getSource().transmit(new AcknowledgeMessage(Acknowledge.INVALID_DATA), "net.aeten.core.test.messenger.server.sender");
+					if (data.getMessage () instanceof Message) {
+						Message recievedMessage = (Message) data.getMessage ();
+						if ( (recievedMessage.getValue () < Message.MIN_VALUE) || (recievedMessage.getValue () > Message.MAX_VALUE)) {
+							data.getSource ().transmit (new AcknowledgeMessage (Acknowledge.INVALID_DATA), "net.aeten.core.test.messenger.server.sender");
 						} else {
-							data.getSource().transmit(new AcknowledgeMessage(Acknowledge.OK), "net.aeten.core.test.messenger.server.sender");
+							data.getSource ().transmit (new AcknowledgeMessage (Acknowledge.OK), "net.aeten.core.test.messenger.server.sender");
 						}
 					}
 					break;
@@ -101,58 +87,58 @@ public class MessengerTest {
 					break;
 				}
 			}
-		}, Messenger.EVENTS.get(MessengerEvent.RECEIVE, Hook.END));
-		server.addEventHandler(new Handler<MessengerEventData<AbstractMessage>>() {
+		}, Messenger.EVENTS.get (MessengerEvent.RECEIVE, Hook.END));
+		server.addEventHandler (new Handler <MessengerEventData <AbstractMessage>> () {
 
 			@Override
-			public void handleEvent(MessengerEventData<AbstractMessage> data) {
-				Logger.log(data.getSource(), LogLevel.INFO, "event={" + data.getEvent() + "}; data={" + data.getMessage() + "}");
+			public void handleEvent (MessengerEventData <AbstractMessage> data) {
+				LOGGER.info ("source=({}); event=({}); data=({})", data.getSource (), data.getEvent (), data.getMessage ());
 			}
-		}, Messenger.EVENTS.get(MessengerEvent.RECEIVE, Hook.END), Messenger.EVENTS.get(MessengerEvent.SEND, Hook.END));
+		}, Messenger.EVENTS.get (MessengerEvent.RECEIVE, Hook.END), Messenger.EVENTS.get (MessengerEvent.SEND, Hook.END));
 
-		client.addEventHandler(new Handler<MessengerEventData<AbstractMessage>>() {
+		client.addEventHandler (new Handler <MessengerEventData <AbstractMessage>> () {
 
 			@Override
-			public void handleEvent(MessengerEventData<AbstractMessage> data) {
-				if ((data.getEvent().getSourceEvent() == MessengerEvent.RECEIVE) && (!(data.getMessage() instanceof AcknowledgeMessage))) {
+			public void handleEvent (MessengerEventData <AbstractMessage> data) {
+				if ( (data.getEvent ().getSourceEvent () == MessengerEvent.RECEIVE) && (! (data.getMessage () instanceof AcknowledgeMessage))) {
 					return;
 				}
-				Logger.log(data.getSource(), LogLevel.INFO, "event={" + data.getEvent() + "}; data={" + data.getMessage() + "}");
+				LOGGER.info ("source=({}); event=({}); data=({})", data.getSource (), data.getEvent (), data.getMessage ());
 			}
-		}, Messenger.EVENTS.get(MessengerEvent.SEND, Hook.END), Messenger.EVENTS.get(MessengerEvent.RECEIVE, Hook.END));
+		}, Messenger.EVENTS.get (MessengerEvent.SEND, Hook.END), Messenger.EVENTS.get (MessengerEvent.RECEIVE, Hook.END));
 
-		server.connect();
-		client.connect();
+		server.connect ();
+		client.connect ();
 
-		Message invalidData = new Message();
-		invalidData.setAcknowledge(Acknowledge.UNSOLLICITED_NEED_ACKNOWLEDGE);
-		invalidData.setValue(10);
+		Message invalidData = new Message ();
+		invalidData.setAcknowledge (Acknowledge.UNSOLLICITED_NEED_ACKNOWLEDGE);
+		invalidData.setValue (10);
 
-		Message highData = new Message();
-		highData.setAcknowledge(Acknowledge.UNSOLLICITED_NEED_ACKNOWLEDGE);
-		highData.setValue(3);
+		Message highData = new Message ();
+		highData.setAcknowledge (Acknowledge.UNSOLLICITED_NEED_ACKNOWLEDGE);
+		highData.setValue (3);
 
-		Message invalidMessage = new Message();
+		Message invalidMessage = new Message ();
 
-		client.transmit(valid(), "net.aeten.core.test.messenger.client.sender");
-		client.transmit(valid(), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
-		client.transmit(valid(), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
-		client.transmit(valid(), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
-		client.transmit(valid(), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
-		client.transmit(valid(), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
-		client.transmit(valid(), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
-		client.transmit(valid(), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
-		client.transmit(highData, "net.aeten.core.test.messenger.client.sender", Priority.HIGH);
-		client.transmit(invalidData, "net.aeten.core.test.messenger.client.sender", Priority.LOW);
-		client.transmit(invalidMessage, "net.aeten.core.test.messenger.client.sender", Priority.HIGH);
+		client.transmit (valid (), "net.aeten.core.test.messenger.client.sender");
+		client.transmit (valid (), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
+		client.transmit (valid (), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
+		client.transmit (valid (), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
+		client.transmit (valid (), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
+		client.transmit (valid (), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
+		client.transmit (valid (), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
+		client.transmit (valid (), "net.aeten.core.test.messenger.client.sender", Priority.LOW);
+		client.transmit (highData, "net.aeten.core.test.messenger.client.sender", Priority.HIGH);
+		client.transmit (invalidData, "net.aeten.core.test.messenger.client.sender", Priority.LOW);
+		client.transmit (invalidMessage, "net.aeten.core.test.messenger.client.sender", Priority.HIGH);
 
-		Thread.currentThread().join();
+		Thread.currentThread ().join ();
 	}
 
-	private static Message valid() {
-		Message valid = new Message();
-		valid.setAcknowledge(Acknowledge.UNSOLLICITED_NEED_ACKNOWLEDGE);
-		valid.setValue(4);
+	private static Message valid () {
+		Message valid = new Message ();
+		valid.setAcknowledge (Acknowledge.UNSOLLICITED_NEED_ACKNOWLEDGE);
+		valid.setValue (4);
 		return valid;
 	}
 }
