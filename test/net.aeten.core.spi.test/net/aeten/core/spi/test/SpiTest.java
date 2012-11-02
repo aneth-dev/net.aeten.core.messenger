abf9ff6502dfd95616fe86a0b8faf25a2655b707
Thomas Pérennou <thomas.perennou@aeten.net>
2012-11-02 09:14:04 +0000

    Configuration annotation must be on package or type declaration

--- test/net.aeten.core.spi.test/net/aeten/core/spi/test/SpiTest.java
+++ test/net.aeten.core.spi.test/net/aeten/core/spi/test/SpiTest.java
@@ -2,6 +2,7 @@ package net.aeten.core.spi.test;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import net.aeten.core.Identifiable;
 import net.aeten.core.spi.Configuration;
 import net.aeten.core.spi.FieldInit;
@@ -13,13 +14,14 @@ public interface SpiTest {
 
 	public void print();
 
-	public static class Foo implements Identifiable {
+	public static class Foo implements
+			Identifiable {
 
 		@FieldInit()
 		private final String id;
 
 		public Foo(@SpiInitializer FooInitializer init) {
-			this.id = init.getId();
+			this.id = init.getId ();
 		}
 
 		@Override
@@ -29,7 +31,10 @@ public interface SpiTest {
 	}
 
 	@Provider(SpiTest.class)
-	public static class SpiTestImpl implements SpiTest {
+	@Configuration(name = "test.yaml",
+						provider = SpiTestImpl.class)
+	public static class SpiTestImpl implements
+			SpiTest {
 
 		@FieldInit()
 		private final String hello;
@@ -39,24 +44,23 @@ public interface SpiTest {
 		private final Map<String, Foo> foos;
 
 		protected SpiTestImpl(@SpiInitializer SpiTestInitializer init) {
-			hello = init.getHello();
-			name = init.getName();
-			foos = init.hasFoos() ? init.getFoos() : new HashMap<>();
+			hello = init.getHello ();
+			name = init.getName ();
+			foos = init.hasFoos () ? init.getFoos () : new HashMap<> ();
 		}
 
 		@Override
 		public void print() {
-			System.out.println(hello + " " + name + "!");
-			for (Map.Entry<String, Foo> foo : foos.entrySet()) {
-				System.out.println(foo.getKey() + ": " + foo.getValue().getIdentifier());
+			System.out.println (hello + " " + name + "!");
+			for (Map.Entry<String, Foo> foo : foos.entrySet ()) {
+				System.out.println (foo.getKey () + ": " + foo.getValue ().getIdentifier ());
 			}
 		}
 
-		@Configuration(name = "test.yaml", provider = SpiTestImpl.class)
 		public static void main(String[] args) {
-			System.out.println("SPI Test");
-			for (SpiTest test : Service.getProviders(SpiTest.class)) {
-				test.print();
+			System.out.println ("SPI Test");
+			for (SpiTest test : Service.getProviders (SpiTest.class)) {
+				test.print ();
 			}
 		}
 	}
